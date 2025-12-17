5ff435fc1df4dbe502a0fc10107579df0de2fae5
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-17 19:46:25 -0300

    heap sort

---
+++ algorithms/heap_sort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// heap sort O(n log n)
+
+void heapify(int arr[], int n, int i) {
+
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heap_sort(int arr[], int n) {
+    
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+void print_array(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {4, 10, 3, 5, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    print_array(arr, n);
+
+    heap_sort(arr, n);
+
+    printf("Sorted array: ");
+    print_array(arr, n);
+    /*
+    Out:
+    Sorted array: 1 3 4 5 10
+    */
+
+    return 0;
+}
