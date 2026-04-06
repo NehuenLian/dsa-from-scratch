99464fe82f6d3de31c3451343ede3c7fa228dd76
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-06 17:53:06 -0300

    refactor queue implementation. remove exit(), use status return and output pointers

--- data_structures/queue/queue_array.c
+++ data_structures/queue/queue_array.c
@@ -8,46 +8,63 @@ int queue[MAX];
 int front = 0;
 int rear = 0;
 
-void enqueue(int element) {
+int enqueue(int element) {
 
     if ((rear + 1) % MAX == front) { // if the result of rear + 1 % 10 == current value of front...
         printf("Queue is full.\n");
-        exit(1);
+        return -1;
     }
     queue[rear] = element;
     rear = (rear + 1) % MAX;
+
+    return 0;
 }
 
-int dequeue() {
+int dequeue(int *value) {
+    if (value == NULL) {
+        return -1;
+    }
 
     if (front == rear) {
         printf("Queue is empty.\n");
-        exit(1);
+        return -1;
     }
 
-    int value = queue[front];
+    *value = queue[front];
     front = (front + 1) % MAX;
-    return value;
+
+    return 0;   
 }
 
-void is_empty() {
+int is_empty() {
     if (front == rear) {
         printf("Queue is empty.\n");
+        return 0;
+    }
+    else {
+        return -1;
     }
 }
 
-void is_full() {
+int is_full() {
     if ((rear + 1) % MAX == front) { // if the result of rear + 1 % 10 == current value of front...
         printf("Queue is full.\n");
+        return 0;
+    }
+    else {
+        return -1;
     }
 }
 
-int peek() {
+int peek(int *value) {
+
     if (front == rear) {
         printf("Queue is empty.\n");
-        exit(1);
+        return -1;
     }
-    return queue[front];
+    *value = queue[front];
+
+    return 0;
 }
 
 int main() {
@@ -58,13 +75,15 @@ int main() {
     enqueue(15);
 
     // take the first added
-    int first_in = peek();
+    int first_in = 0;
+    peek(&first_in);
     printf("First added: %d\n", first_in);
 
     // dequeue the three elements added
-    dequeue();
-    dequeue();
-    dequeue();
+    int dummy = 0;
+    dequeue(&dummy);
+    dequeue(&dummy);
+    dequeue(&dummy);
 
     // verify status
     is_empty();
