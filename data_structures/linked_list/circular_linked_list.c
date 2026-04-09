148f1720ec146655d30ed2efc4990cc19ba2bdb5
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-09 00:09:20 -0300

    refactor insert_node and print_list functions for improved error handling and fixing memory leaks

--- data_structures/linked_list/circular_linked_list.c
+++ data_structures/linked_list/circular_linked_list.c
@@ -11,53 +11,61 @@ struct Node {
     struct Node *next;
 };
 
-struct Node* insert_node(struct Node *head, int value) {
+int insert_node(struct Node **head, int value) {
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         printf("Memory allocation failed.\n");
-        exit(1);
-    }
+        return -1;
+    } 
 
     new_node->value = value;
 
-    if (head == NULL) {
+    if (*head == NULL) {
         new_node->next = new_node; // if there's only one node, it will point itself
-        return new_node;
+        *head = new_node;
+        return 0;
     }
 
-    struct Node *current = head;
-    while (current->next != head) {
+    struct Node *current = *head;
+    while (current->next != *head) {
         current = current->next; // go to the last 
     }
 
     current->next = new_node; // add new node at the end
-    new_node->next = head; // close the circle
+    new_node->next = *head; // close the circle
 
-    return head;
+    return 0;
 }
 
-void print_list(struct Node *head) {
+int print_list(struct Node *head) {
     struct Node *current = head;
+    if (current == NULL) {
+        return -1;
+    }
     
     do {
         printf("%d -> ", current->value);
         current = current->next; 
     } while (current != head); // do {} while; for skip first iteration, that will be false 
 
-    printf("NULL\n");
+    printf("HEAD\n");
+    return 0;
 }
 
 void free_linked_list(struct Node *head) {
-    if (!head) return;
+    if (!head) {
+        return;
+    }
 
-    struct Node *current = head;
+    struct Node *current = head->next;
     struct Node *next_node; // the next node after the current
 
-    do {
+    while (current != head) {
         next_node = current->next;
         free(current);
-        current = next_node; // current was freed so we set the next node to free
-    } while (current != head); // do {} while; for skip first iteration, that will be false
+        current = next_node;  // current was freed so we set the next node to free
+    }
+    free(head);
 
     printf("Free memory process completed.\n");
 }
@@ -65,9 +73,9 @@ void free_linked_list(struct Node *head) {
 int main() {
     struct Node *linked_list = NULL;
     
-    linked_list = insert_node(linked_list, 5);
-    linked_list = insert_node(linked_list, 10);
-    linked_list = insert_node(linked_list, 15);
+    insert_node(&linked_list, 5);
+    insert_node(&linked_list, 10);
+    insert_node(&linked_list, 15);
     /*
     Out: 5 -> 10 -> 15 -> NULL
     */
