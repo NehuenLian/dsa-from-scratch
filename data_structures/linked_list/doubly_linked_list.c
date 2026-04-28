a67b89ab1124861bce7a7f9cebff67f234aefb49
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-28 02:06:06 -0300

    adjust type returns for consistence and use pointer to pointer to access to linked_list. avoid variable shadowing

--- data_structures/linked_list/doubly_linked_list.c
+++ data_structures/linked_list/doubly_linked_list.c
@@ -13,60 +13,65 @@ struct LinkedList {
     struct Node *tail;
 };
 
-struct LinkedList* insert_at_tail(struct LinkedList *linked_list, int value) {
+int insert_at_tail(struct LinkedList **linked_list, int value) {
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         printf("Memory allocation failed.\n");
-        exit(1);
+        return -1;
     }
 
     new_node->value = value;
     new_node->next = NULL;
     new_node->prev = NULL;
 
-    if (linked_list == NULL) {
+    if (*linked_list == NULL) {
 
-        linked_list = malloc(sizeof(struct LinkedList));
+        struct LinkedList *new_linked_list = malloc(sizeof(struct LinkedList));
 
-        linked_list->head = new_node;
-        linked_list->tail = new_node;
+        new_linked_list->head = new_node;
+        new_linked_list->tail = new_node;
         new_node->prev = NULL;
 
-        return linked_list;
-    } else {
-        new_node->prev = linked_list->tail;
-        linked_list->tail->next = new_node;
-        linked_list->tail = new_node;
+        *linked_list = new_linked_list;
 
+        return 0;
+    } else {
+        new_node->prev = (*linked_list)->tail;
+        (*linked_list)->tail->next = new_node;
+        (*linked_list)->tail = new_node;
+
+        return 0;
     }
-    return linked_list;
+    return 0;
 }
 
-struct LinkedList* insert_at_head(struct LinkedList *linked_list, int value) {
+int insert_at_head(struct LinkedList **linked_list, int value) {
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         printf("Memory allocation failed.\n");
-        exit(1);
+        return -1;
     }
 
     new_node->value = value;
     new_node->prev = NULL;
 
-    if (linked_list == NULL) {
+    if (*linked_list == NULL) {
 
-        linked_list = malloc(sizeof(struct LinkedList));
+        struct LinkedList *new_linked_list = malloc(sizeof(struct LinkedList));
 
-        linked_list->head = new_node;
-        linked_list->tail = new_node;
+        new_linked_list->head = new_node;
+        new_linked_list->tail = new_node;
 
-        return linked_list;
+        return 0;
+    } 
+    else {
+        new_node->next = (*linked_list)->head;
+        (*linked_list)->head->prev = new_node;
+        (*linked_list)->head = new_node;
 
-    } else {
-        new_node->next = linked_list->head;
-        linked_list->head->prev = new_node;
-        linked_list->head = new_node;
+        return 0;
     }
-    return linked_list;
+    return 0;
 }
 
 void print_list(struct LinkedList *linked_list) {
@@ -99,10 +104,10 @@ int main() {
 
     struct LinkedList *linked_list = NULL;
 
-    linked_list = insert_at_tail(linked_list, 10);
-    linked_list = insert_at_tail(linked_list, 15);
-    linked_list = insert_at_tail(linked_list, 20);
-    linked_list = insert_at_head(linked_list, 5);
+    insert_at_tail(&linked_list, 10);
+    insert_at_tail(&linked_list, 15);
+    insert_at_tail(&linked_list, 20);
+    insert_at_head(&linked_list, 5);
     /*
     Out: 5 -> 10 -> 15 -> 20 -> NULL
     */
