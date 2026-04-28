13294b424a8450bbb64c6309cc557d0f48f20469
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-28 01:57:52 -0300

    adjust type returns for consistence and use pointer to pointer to access to linked_list

--- data_structures/linked_list/classic_linked_list.c
+++ data_structures/linked_list/classic_linked_list.c
@@ -7,7 +7,7 @@ struct Node {
     struct Node *next;
 };
 
-struct Node* insert_node(struct Node *head, int value) {
+int insert_node(struct Node **head, int value) {
     /*
     Inserts a node at the end of the list:
     head -> new_node -> null
@@ -15,23 +15,25 @@ struct Node* insert_node(struct Node *head, int value) {
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         printf("Memory allocation failed.\n");
-        exit(1);
+        return -1;
     }
 
     new_node->value = value;
     new_node->next = NULL;
 
-    if (head == NULL) {
-        return new_node; // if head is null return new_node as the first node
+    if (*head == NULL) {
+        *head = new_node; // if head is null return new_node as the first node
+        return 0;
     }
 
-    struct Node *current = head;
+    struct Node *current = *head;
     while (current->next != NULL) {
         current = current->next;
     }
     current->next = new_node; // insert new node an the end
+    *head = current;
 
-    return head; // always return the head (first node)
+    return 0;
 }
 
 void print_list(struct Node *head) {
@@ -56,9 +58,9 @@ void free_linked_list(struct Node *head) {
 int main() {
     struct Node *linked_list = NULL;
     
-    linked_list = insert_node(linked_list, 5);
-    linked_list = insert_node(linked_list, 10);
-    linked_list = insert_node(linked_list, 15);
+    insert_node(&linked_list, 5);
+    insert_node(&linked_list, 10);
+    insert_node(&linked_list, 15);
     /*
     Out: 5 -> 10 -> 15 -> NULL
     */
